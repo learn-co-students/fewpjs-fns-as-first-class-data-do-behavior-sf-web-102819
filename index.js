e56e2e720d96592375709accf17b933dd67a3ea6
Your_Name <joseph161216@gmail.com>
2019-12-09 15:02:43 -0800

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(string) {
+  const arr = string.split()
+  const time = parseInt(arr[0], 10)
+  console.log(arr[0])
+  if(time <= 12) {
+    return "Good Morning";
+  } else if(time >= 12 && time <= 17) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+  console.log(string)
+  const greet = document.querySelector("#greeting");
+  greet.innerText = string;
+}
